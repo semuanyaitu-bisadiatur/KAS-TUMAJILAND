a832f293b5191dfdbd0d9b74cc58887116bbe0af
semuanyaitu-bisadiatur <semuanyaitu@gmail.com>
2026-05-01 11:11:54 +0700

    Update version.js

--- version.js
+++ version.js
@@ -2,7 +2,7 @@
 // 🔥 EDIT VERSI DI FILE INI SAJA!
 // ============================================
 
-const APP_VERSION = '1.0.5';  // ← GANTI INI SAJA SETIAP UPDATE
+const APP_VERSION = '1.0.6';  // ← GANTI INI SAJA SETIAP UPDATE
 
 // Auto-generate cache name dari versi
 const CACHE_NAME = 'kas-perumahan-' + APP_VERSION.replace(/\./g, '-');
